fb414ba58bab0e82838b4c94b64fbe86c553ee02
Olle Fagerlin <olle.fagerlin@gmail.com>
2020-10-22 14:23:50 +0200

    login logic

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,17 +1,59 @@
 ﻿// We've used jQuery since it came bundled in the installation.
 // Feel free to change to change to a JS-lib of your choice.
 
+function displayMonth(month) {
+  $(".calendar_month header")
+    .empty()
+    .append("<h3>" + month + "</h3>");
+}
+
+function displayCalendar(days) {
+  console.log(days);
+
+  $("#calendar .calendar_month .dates").empty();
+
+  for (let i = 0; i <= days.length; i++) {
+    $("#calendar .calendar_month .dates").append(
+      '<div class="date alert-success"><span class="registered">' +
+        days[i].currentCapacity +
+        "/" +
+        days[i].maxCapacity +
+        '</span><span class="day_of_month">' +
+        (i + 1) +
+        "</span></div>"
+    );
+  }
+}
+
 function login(user, pass) {
-  // alert(user + " " + pass);
-  $("#login").fadeOut(function () {
-    $("#calendar").fadeIn();
+  $("#login_form").disabled;
+
+  $.ajax(
+    "https://officemancer.azurewebsites.net/user/login?username=" +
+      user +
+      "&password=" +
+      pass +
+      ""
+  ).done(function (data) {
+    if (data == "Success") {
+      $("#login").fadeOut(function () {
+        $("#calendar").fadeIn(function () {
+          $("#logged_in_as").append(user);
+
+          $.ajax(
+            "https://officemancer.azurewebsites.net/user/getmonth?officeid=1&month=10&year=2020"
+          ).done(function (data) {
+            displayMonth(data.month);
+            displayCalendar(data.days);
+          });
+        });
+      });
+    }
   });
 }
 
 $(document).ready(function () {
   $("#login_form").submit(function (e) {
-    // alert("Run login script");
-
     const user = $("#email").val();
     const pass = $("#password").val();
 
