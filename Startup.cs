421ef611b5edf75a6e087e462f12ea53a496c0f2
joiny <joiny4u@hotmail.com>
2020-10-22 12:00:03 +0200

    chf

--- Startup.cs
+++ Startup.cs
@@ -95,7 +95,7 @@ namespace Officemancer
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors(MyAllowSpecificOrigins);
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
