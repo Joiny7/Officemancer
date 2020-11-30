86e911c01e65f97ec3cedfdf7879d018b7e3721e
denissopovic <denis@sopovic.com>
2020-11-30 09:29:10 +0100

    commit

--- frontend/src/components/Content/Content.js
+++ frontend/src/components/Content/Content.js
@@ -3,7 +3,6 @@ import styles from './Content.module.css';
 import axios from 'axios';
 import Moment from 'moment'; 
 
-
 const Content = () => {
   const [showMonth, setShowMonth] = useState('');
   const [showDay, setShowDay] = useState([]);
@@ -11,39 +10,56 @@ const Content = () => {
   axios.post('api/User/GetMonth?officeID=1&month=11&year=2020',{}).then(response => {
     setShowDay(response.data.days);
     setShowMonth(response.data.month);
-    //console.log(response.data);
   }); 
+
+  console.log(showMonth);
 
   Moment.locale('sv');
 
-  return (
-    <div className={styles.Content}>
-      <div className="ui grid">
-        <div className="ui center aligned header icon">
-          <h1 className="center">{showMonth}</h1>
+
+  if (!showMonth){
+    return (
+      <div className={styles.Content}>
+        <div class="ui segment">
+          <div class="ui active inverted dimmer">
+        <div class="ui text loader">Loading</div>
         </div>
-        <div className="five column row">
-        <div className="column">mån</div>
-        <div className="column">tis</div>
-        <div className="column">ons</div>
-        <div className="column">tor</div>
-        <div className="column">fre</div>
-      </div>  
-      <div className="five column row cards">
-          {showDay.map((item, i) => {
-            return (
-                <div className={`ui column card ${styles.Contentcal}`} key={item.date}>
-                  <h3>{Moment(item.date).format('D')}</h3>
-                  <div>Upptagna platser: { item.currentCapacity }</div>
-                </div>
-            )  
-            
-          })}
+          <p><br /><br /><br /><br /></p>
         </div>
       </div>
-      
-    </div>
-  );
+    );
+  }
+  else {
+    return (
+      <div className={styles.Content}>
+        <div className="ui grid">
+          <div className="ui center aligned header icon">
+            <h1 className="center">{showMonth}</h1>
+          </div>
+          <div className="five column row">
+          <div className="column">mån</div>
+          <div className="column">tis</div>
+          <div className="column">ons</div>
+          <div className="column">tor</div>
+          <div className="column">fre</div>
+        </div>  
+        <div className="five column row cards">
+            {showDay.map((item, i) => {
+              return (
+                  <div className={`ui column card ${styles.Contentcal}`} key={item.date}>
+                    <h3>{Moment(item.date).format('D')}</h3>
+                    <div>Upptagna platser: { item.currentCapacity }</div>
+                  </div>
+              )  
+              
+            })}
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
+
 };
 
 
